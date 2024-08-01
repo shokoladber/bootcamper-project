046bc2bab7394f8637246b4ba0d087c36038949c
Allen <michael.a.allen@afs.com>
2024-08-01 15:42:02 -0500

    Cleaned up models and adjusted corresponding view gamesPage.

--- mgl-webapp/src/main/java/com/organization/mvcproject/model/Company.java
+++ mgl-webapp/src/main/java/com/organization/mvcproject/model/Company.java
@@ -1,17 +1,12 @@
 
 package com.organization.mvcproject.model;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.stereotype.Component;
-
-//TODO 1.0 remove @Component annotation, it is not used
 public class Company {
 
 	private Long id;
 	private String name;
-	//TODO 1.0 this List doesn't need to be initialized at declaration. 
 	private List<Game> gamesMade;
 
 	public Long getId() {
