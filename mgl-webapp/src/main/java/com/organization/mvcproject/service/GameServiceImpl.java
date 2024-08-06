9bad179b8e036aedfaf3a552c68bc9bfeb7bcbba
Allen <michael.a.allen@afs.com>
2024-08-06 08:50:33 -0500

    Updated methods in GameServiceImpl. Retrieve games and add game functionality now works.

--- mgl-webapp/src/main/java/com/organization/mvcproject/service/GameServiceImpl.java
+++ mgl-webapp/src/main/java/com/organization/mvcproject/service/GameServiceImpl.java
@@ -1,21 +1,28 @@
 package com.organization.mvcproject.service;
 
+import com.organization.mvcproject.api.dao.GameDAO;
 import com.organization.mvcproject.api.service.GameService;
+import com.organization.mvcproject.dao.GameDAOImpl;
 import com.organization.mvcproject.model.GameImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class GameServiceImpl implements GameService {
+
+	@Autowired
+	private GameDAO gameDAOImpl;
+
 	@Override
 	public List<GameImpl> retrieveAllGames() {
-		return List.of();
+		return gameDAOImpl.findAllGames();
 	}
 
 	@Override
 	public GameImpl saveGame(GameImpl game) {
-		return null;
+		return gameDAOImpl.saveGame(game);
 	}
 
 	@Override
